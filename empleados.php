67c6000b272bef7f6c2b214655b23841bfd7c543
Ayrton-33 <ayrton.94acevedo@gmail.com>
2022-07-22 20:52:34 -0300

    cambios

--- empleados.php
+++ empleados.php
@@ -1,27 +1,32 @@
 <?php
-ini_set ("display_errors", 1);
+ini_set("display_errors", 1);
 ini_set("display_startup-errors", 1);
 error_reporting(E_ALL);
 
-$aEmpleados= array ();
-$aEmpleados[]=array ("dni"=>38987765,"nombre"=>"David Garcia","bruto"=>85000.30);
-$aEmpleados[]=array ("dni"=>5894985,"nombre"=>"Ana del Valle","bruto"=>90000);
-$aEmpleados[]=array ("dni"=>25678099,"nombre"=>"Andrés Perez","bruto"=>100000);
-$aEmpleados[]=array ("dni"=>17345675,"nombre"=>"Victoria Luz","bruto"=>70000);
+$aEmpleados = array();
+$aEmpleados[] = array("dni" => 38987765, "nombre" => "David Garcia", "bruto" => 85000.30);
+$aEmpleados[] = array("dni" => 5894985, "nombre" => "Ana del Valle", "bruto" => 90000);
+$aEmpleados[] = array("dni" => 25678099, "nombre" => "Andrés Perez", "bruto" => 100000);
+$aEmpleados[] = array("dni" => 17345675, "nombre" => "Victoria Luz", "bruto" => 70000);
 
-
-
+//Definicion
+function calcularNeto($bruto)
+{
+    return $bruto - ($bruto * 0.17);
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <title>Empleados</title>
 </head>
+
 <body>
     <main class="container">
         <div class="row">
@@ -32,29 +37,33 @@ $aEmpleados[]=array ("dni"=>17345675,"nombre"=>"Victoria Luz","bruto"=>70000);
         <div class="row">
             <div class="col-12">
                 <table class="table table-hover border">
-                <thead>    
-                    <tr>
-                        <th>Dni</th>
-                        <th>Nombre</th>
-                        <th>Sueldo</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <tbody>
-                        <?php foreach ($aEmpleados as $empleado){ ?>
+                    <thead>
+                        <tr>
+                            <th>Dni</th>
+                            <th>Nombre</th>
+                            <th>Sueldo</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($aEmpleados as $empleado) { ?>
                             <tr>
-                                <td><?php echo $empleado ["dni"]; ?></td>
-                                <td><?php echo $empleado ["nombre"]; ?></td>
-                                <td><?php echo $empleado ["bruto"]; ?></td>
+                                <td><?php echo $empleado["dni"]; ?></td>
+                                <td><?php echo mb_strtoupper($empleado["nombre"]); ?></td>
+                                <td><?php echo number_format(calcularNeto($empleado["bruto"]), 2, ",", "."); ?></td>
                             </tr>
-                            <?php } ?>
-                </tbody>
-                </div>
+                        <?php } ?>
+                    </tbody>
+                </table>
             </div>
-
+        </div>
+        <div class="row">
+            <div class="col-12 pt-3">
+                <p>Cantidad de empleados activos: <?php echo count($aEmpleados); ?></p>
+            </div>
         </div>
 
     </main>
-    
+
 </body>
+
 </html>
